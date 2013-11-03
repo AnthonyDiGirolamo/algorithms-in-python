5d7ae67708695ded74fcd0e51f36af64a3d87f1f
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2013-11-03 11:31:13 -0500

    added an insertionsort doctest

--- insertionsort.py
+++ insertionsort.py
@@ -2,6 +2,14 @@
 # coding: utf-8
 
 def insertionsort(array):
+    """Run insertionsort on a list
+
+    >>> a = [45,67,2,3,5,7,898,9,6,5,4,33,3,35566,1]
+    >>> insertionsort(a)
+    >>> print(a)
+    [1, 2, 3, 3, 4, 5, 5, 6, 7, 9, 33, 45, 67, 898, 35566]
+    """
+
     for i in range(1, len(array)):
         value_to_insert = array[i]
         hole_index = i
@@ -10,3 +18,6 @@ def insertionsort(array):
             hole_index -= 1
         array[hole_index] = value_to_insert
 
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
