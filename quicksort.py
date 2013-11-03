fba0103887f52c58cc537da4377e92090eee2c6e
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2013-11-03 09:11:33 -0500

    added quicksort.py

---
+++ quicksort.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+def quicksort_recursive(array):
+    if len(array) <= 1:
+        return array
+    less     = []
+    greater  = []
+    midpoint = int(len(array)/2)
+    pivot    = array[midpoint]
+    array    = array[:midpoint] + array[midpoint+1:]
+    for item in array:
+        if item <= pivot:
+            less.append(item)
+        else:
+            greater.append(item)
+    return (quicksort_recursive(less) + [pivot] + quicksort_recursive(greater))
+
+a = [9, 2, 5, 1, 4, 6, 7, 59493, 3, 3929, 2, 3, 4, ]
+
+print(quicksort_recursive(a))
+
+def partition(array, left_index, right_index, pivot_index):
+    pivot_value        = array[pivot_index]
+    array[pivot_index], array[right_index] = array[right_index], array[pivot_index]
+    # pivot_value        = array[pivot_index]
+    # array[pivot_index] = array[right_index]
+    # array[right_index] = pivot_value
+    store_index        = left_index
+    for i in range(left_index, right_index):
+        if array[i] <= pivot_value:
+            array[store_index], array[i] = array[i], array[store_index]
+            # store_value        = array[store_index]
+            # array[store_index] = array[i]
+            # array[i]           = store_value
+            store_index += 1
+    array[store_index], array[right_index] = array[right_index], array[store_index]
+    # store_value        = array[store_index]
+    # array[store_index] = array[right_index]
+    # array[right_index] = store_value
+    return store_index
+
+def quicksort(array, left_index, right_index):
+    if left_index < right_index:
+        pivot_index = left_index + int((right_index-left_index)/2)
+        new_pivot_index = partition(array, left_index, right_index, pivot_index)
+        quicksort(array, left_index, new_pivot_index - 1)
+        quicksort(array, new_pivot_index + 1, right_index)
+
+a = [9, 2, 5, 1, 4, 6, 7, 59493, 3, 3929, 2, 3, 4]
+
+quicksort(a, 0 , len(a)-1)
+
+print(a)
+
