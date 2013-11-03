d22da219e40d5e6e47ac20da1be50369afed0c63
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2013-11-03 12:22:42 -0500

    added heapsort and finalized rand_array.pickle

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 algorithms-in-python
 ====================
+
+Test Runs
+---------
+
+### Sorting 100,000 Floats in  Python 2.7.5, 3.3.2 and PyPy 2.1.0
+
+    $ python test_sorting.py
+    2.7.5 (default, Oct 25 2013, 11:07:52)
+    [GCC 4.2.1 Compatible Apple LLVM 5.0 (clang-500.2.75)]
+    python sorted():
+    0.430683135986
+    quicksort_recursive:
+    1.60994386673
+    quicksort with in place partitioning:
+    1.55121803284
+    top_down_mergesort:
+    2.62138414383
+    heapsort:
+    3.60810089111
+
+    $ python3 test_sorting.py
+    3.3.2 (default, Oct 21 2013, 12:01:11)
+    [GCC 4.2.1 Compatible Apple LLVM 4.2 (clang-425.0.28)]
+    python sorted():
+    0.1217644480002491
+    quicksort_recursive:
+    1.187993325998832
+    quicksort with in place partitioning:
+    1.2979137350012024
+    top_down_mergesort:
+    2.366787152001052
+    heapsort:
+    3.2705076370002644
+
+    $ /usr/local/Cellar/pypy/2.1.0/libexec/bin/pypy test_sorting.py
+    2.7.3 (480845e6b1dd, Jul 31 2013, 10:58:28)
+    [PyPy 2.1.0 with GCC 4.2.1 Compatible Clang Compiler]
+    python sorted():
+    0.372516155243
+    quicksort_recursive:
+    0.644681930542
+    quicksort with in place partitioning:
+    0.456494092941
+    top_down_mergesort:
+    0.737778902054
+    heapsort:
+    0.453850030899
--- test_sorting.py
+++ test_sorting.py
@@ -3,7 +3,7 @@
 
 def write_random_data():
     # write out a random list of floats:
-    a = [random.random() for i in range(0, 10000)]
+    a = [random.random() for i in range(0, 100000)]
     with open("rand_array.pickle", "wb") as f:
         pickle.dump(a, f, pickle.HIGHEST_PROTOCOL)
 
@@ -23,8 +23,11 @@ if __name__ == '__main__':
 
     print(sys.version)
 
-    print("insertionsort:")
-    print(timeit.timeit('data = random_data() ; insertionsort(data)', setup="from __main__ import random_data ; from insertionsort import insertionsort", number=3))
+    # print("insertionsort:")
+    # print(timeit.timeit('data = random_data() ; insertionsort(data)', setup="from __main__ import random_data ; from insertionsort import insertionsort", number=3))
+
+    print("python sorted():")
+    print(timeit.timeit('data = random_data() ; sorted(data)', setup="from __main__ import random_data", number=3))
 
     print("quicksort_recursive:")
     print(timeit.timeit('data = random_data() ; quicksort_recursive(data)', setup="from __main__ import random_data ; from quicksort import quicksort_recursive", number=3))
@@ -32,9 +35,13 @@ if __name__ == '__main__':
     print("quicksort with in place partitioning:")
     print(timeit.timeit('data = random_data() ; quicksort(data,0 , len(data)-1)', setup="from __main__ import random_data ; from quicksort import partition, quicksort", number=3))
 
-    print("mergesort:")
-    print(timeit.timeit('data = random_data() ; mergesort(data)', setup="from __main__ import random_data ; from mergesort import mergesort, merge", number=3))
+    # this implementation is very slow
+    # print("mergesort:")
+    # print(timeit.timeit('data = random_data() ; mergesort(data)', setup="from __main__ import random_data ; from mergesort import mergesort, merge", number=3))
 
     print("top_down_mergesort:")
     print(timeit.timeit('data = random_data() ; top_down_mergesort(data)', setup="from __main__ import random_data ; from mergesort import top_down_merge, top_down_mergesort, top_down_mergesort_split_merge", number=3))
+
+    print("heapsort:")
+    print(timeit.timeit('data = random_data() ; heapsort(data)', setup="from __main__ import random_data ; from heapsort import heapsort, heapify, sift_down", number=3))
 
