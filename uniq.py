ed38dbdef6d5e58f1d93635983f60664c7c58750
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2015-12-01 10:42:12 -0500

    fix some small issues with uniq.py
    - has_key is not in python3
    - better variable names

--- uniq.py
+++ uniq.py
@@ -1,4 +1,5 @@
-def uniq(list, keep_dupes=1):
+#!/usr/bin/env python
+def uniq(list, allowed_duplicate_count=1):
     """return uniq elements in a list
 
     >>> test_list = [ 'no', 'way', 'way', 'friend', 'hello', 'there', 'my', 'way', 'friend' ]
@@ -12,14 +13,15 @@ def uniq(list, keep_dupes=1):
 
     d = {}
     for index, item in enumerate(list):
-        if not d.has_key(item):
+        if item not in d:
             d[item] = [ [index, item] ]
-        elif len(d[item]) < keep_dupes:
+        elif len(d[item]) < allowed_duplicate_count:
             d[item].append( [index, item] )
 
-    return [value for index, value in sorted([item for key in d.values() for item in key], key=lambda item: item[0])]
+    return [ value for index, value in
+             sorted([ word_index_pair for word_index_list in d.values() for word_index_pair in word_index_list ], # flatten one level deep
+                    key=lambda item: item[0]) ] # sort on the first value
 
 if __name__ == "__main__":
     import doctest
     doctest.testmod(verbose=True)
-
